121d7f44e6ec5e52fec8f1acc765eb7405c0505b
akshayjain8149 <62173071+akshayjain8149@users.noreply.github.com>
2024-05-30 03:51:09 +0000

    DataTypes

---
+++ js_code/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 4)  //we are using nodejs, not browser
+
+console .log(3 + 7) // code redibility should be high
+console.log("akshay")
+
+let name = "aj"
+let age =27
+let isLoggedIn = false
+
+// number => 2 to power 53
+//biginit
+//string => ""
+// boolean => true / false
+// null => it is standalone value / empty
+//undefined => is not assigned value
+//symbol => uniqueness find
+
+//object
+
+console.log(typeof("aj"));
+
+console.log(typeof(age));
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
---
+++ js_code/varibles.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "akshay@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 not allowed
+
+accountEmail = "aj@google.com"
+var accountPassword = "1281"
+accountCity = "mumbai"
+
+console.log(accountId);
+console.table([accountId ,accountEmail,accountPassword,accountCity.accountState])
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
