d249de46380c74a5467251ece9d4a887f841c7f1
akshayjain8149 <62173071+akshayjain8149@users.noreply.github.com>
2024-06-04 04:13:32 +0000

    Operation

---
+++ conversionOperation.js
@@ -0,0 +1,27 @@
+/*let value = 3
+let negvalue = -value
+
+console.log(negvalue)
+
+let str1 = "Hello"
+let str2 =" jain"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 +"2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3 + 4) * 5 % 3);
+
+console.log(+ true);
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1);*/
+
+let gamecounter = 100
+gamecounter ++;
+console.log(gamecounter);
