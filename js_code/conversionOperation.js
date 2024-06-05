670dc1fcba6463596564ee69a162c47b9a64a8e9
akshayjain8149 <62173071+akshayjain8149@users.noreply.github.com>
2024-06-05 03:48:38 +0000

    conversion opration

--- js_code/conversionOperation.js
+++ js_code/conversionOperation.js
@@ -20,8 +20,22 @@ console.log(+"")
 
 let num1, num2, num3
 num1 = num2 = num3 = 2 + 2
-console.log(num1);*/
+console.log(num1);
 
 let gamecounter = 100
 gamecounter ++;
-console.log(gamecounter);
+console.log(gamecounter);*/
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null <= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+console.log(undefined <= 0);
