a7938a9f3c66ae704bee5d1960a9567ae34e4812
akshayjain8149 <62173071+akshayjain8149@users.noreply.github.com>
2024-06-08 04:04:46 +0000

    memory

--- js_code/datatypesSummary.js
+++ js_code/datatypesSummary.js
@@ -2,7 +2,7 @@
 
 //7 types = Sring , Number , Boolean , null ,undefined, Symbol(Any value manke Unique), BigInt 
 //Example
-const score = 100 //number
+/*const score = 100 //number
 const scorevalue =100.3 //number
 const isLoggedIn =false //Boolean
 const outsidetemp = null //null
@@ -24,4 +24,29 @@ let myObj={name : "akshay", age:28}
 
 const myFunction = function () {
     console.log("Hello World")
+}*/
+
+
+//--------------------------------------------
+
+//Stack Memory(Primitive) and heap memory(Non-Primitive)
+
+//Stack Example
+/*let myStack = "Akshayjain"
+let mystack1 = myStack;
+mystack1 = "jainkashay"
+console.log(myStack);
+console.log(mystack1);*/
+
+//Heap Example
+let userOne ={
+    email: "akshay@gmail.com",
+    upi: "user@ybl"
 }
+let usertwo = userOne
+console.log(usertwo);
+console.log(userOne.email);
+usertwo.email = "aj@gmial.com"
+console.log(userOne.email);
+console.log(usertwo.email);
+
