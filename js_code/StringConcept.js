d30178f60f3a6c0483c8d34cf37f93fa59f755d5
akshayjain8149 <62173071+akshayjain8149@users.noreply.github.com>
2024-06-11 04:28:31 +0000

    concept

--- js_code/StringConcept.js
+++ js_code/StringConcept.js
@@ -1,4 +1,31 @@
-const name = "akshay"
+/*const name = "akshay"
 const repocount = 50
 console.log("my name is",name + "repocount is",repocount);
 console.log(`hello my name is ${name} and repocount is ${repocount}`);
+
+const gameName = new String('hiteshhc')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());*/
+const gameName = new String('hitesh-hc')
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const another = gameName.slice(-7,4)
+console.log(another);
+
+const newStringOne = " akshay  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https:/akshay.com/akshay%20jain"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes("akshay"))
+
+console.log(gameName.split('-'))
